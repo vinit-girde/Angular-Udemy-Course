09d718fa04fe8bdf86b9d13abc4fba2a282c73db
vinit-girde <vinitgirde@gmail.com>
2025-03-11 19:00:52 +0530

    feat: added  dummy users image and data and binded data of user component.

--- task-management-app/src/app/user/user.component.ts
+++ task-management-app/src/app/user/user.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+// helper variable to randomly select index from dummy users data
+let dummyUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[dummyUserIndex];
 
+  // gettter property for computing dynamic user image profile image which can directly be used inside src attribute in property binding without parenthesis.
+  get imagePath() {
+    return `assets/users/` + this.selectedUser.avatar;
+  }
 }
