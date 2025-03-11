43501130035605de68ea92e58b94c3001ad340b6
vinit-girde <vinitgirde@gmail.com>
2025-03-11 13:53:38 +0530

    feat: Created custom header component

--- task-management-app/src/app/app.component.ts
+++ task-management-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'task-management-app';
