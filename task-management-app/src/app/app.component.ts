a2721721f34c3a27fda978e9bea1230776fbecc9
vinit-girde <vinitgirde@gmail.com>
2025-03-11 19:59:57 +0530

    feat: Loading data of users from app component to users

--- task-management-app/src/app/app.component.ts
+++ task-management-app/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, UserComponent],
@@ -10,4 +12,5 @@ import { UserComponent } from './user/user.component';
 })
 export class AppComponent {
   title = 'task-management-app';
+  users = DUMMY_USERS;
 }
--- task-management-app/src/app/user/user.component.ts
+++ task-management-app/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 // helper variable to randomly select index from dummy users data
@@ -11,10 +11,16 @@ let dummyUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[dummyUserIndex];
+  //now better way is to pass it from outside instead of finding index so use @Input decorator
+  // selectedUser = DUMMY_USERS[dummyUserIndex];
+
+  @Input() avatar!: string;
+  @Input() name!: string;
 
   // gettter property for computing dynamic user image profile image which can directly be used inside src attribute in property binding without parenthesis.
   get imagePath() {
-    return `assets/users/` + this.selectedUser.avatar;
+    return `assets/users/` + this.avatar;
   }
+  // Function to perform action on clicking of user
+  onSelectedUser() {}
 }
