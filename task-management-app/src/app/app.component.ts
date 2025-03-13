bc1de3c616e2cb5f7d9577e72dfdb6cb8de8f54e
vinit-girde <vinitgirde@gmail.com>
2025-03-13 18:46:53 +0530

    feat: Tasks component created and added in app template file

--- task-management-app/src/app/app.component.ts
+++ task-management-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
