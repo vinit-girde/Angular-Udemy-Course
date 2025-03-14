654ce3ce6a34763a8abe5c9d208f0ba065d7c36c
vinit-girde <vinitgirde@gmail.com>
2025-03-14 23:39:31 +0530

    feat: dummy tasks array added and filtered according to individual users task

--- task-management-app/src/app/tasks/tasks.component.ts
+++ task-management-app/src/app/tasks/tasks.component.ts
@@ -8,5 +8,39 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+  // user name
   @Input() name?: string;
+  // user ID
+  @Input({ required: true }) userId?: string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  // getter to compute individual's user tasks from dummy tasks .
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
